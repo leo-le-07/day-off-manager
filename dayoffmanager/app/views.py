d9bb777365bf06fa1b586a79bee4d01269b9f2f5
Leo <coder.leo.le@gmail.com>
2018-12-23 22:04:22 +0700

    Request page with dynamic loading data & Apply Bootstrap theme

--- dayoffmanager/app/views.py
+++ dayoffmanager/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def request_list(request):
+  user = User.objects.first()
+  return render(request, 'app/request_list.html', {'user': user})
